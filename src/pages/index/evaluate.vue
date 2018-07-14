99f97ff0eded2483916269e8fa158d6773fc2df4
邓蕾 <542284263@qq.com>
2018-07-14 17:26:42 +0800

    弹窗

---
+++ src/pages/index/evaluate.vue
@@ -0,0 +1,110 @@
+<template>
+  <!-- 商品评价 -->
+  <div>
+    <div class="rightTop">
+        商品评价
+    </div>
+    <div class="flex evaList">
+        <div class="shopInfo">
+          <div><img src="../../assets/image/LOGO.png" style="width:135px;height:135px;border:1px solid #ddd;"/></div>
+          <h4>泰国制作灯泡</h4>
+          <p>规格：灯饰</p>
+        </div>
+        <div class="shopText">
+            <div class="flex shopStar">
+              <div>评分</div>
+              <div>
+                <img src="../../assets/image/评价黄.png"/><img src="../../assets/image/评价黄.png"/><img src="../../assets/image/评价灰.png"/>
+                <img src="../../assets/image/评价灰.png"/><img src="../../assets/image/评价灰.png"/>
+              </div>
+              <div>非常满意</div>
+            </div>
+
+            <div>
+              <textarea placeholder="填写高质量的评价，即可参与抽奖哦"></textarea>
+            </div>
+
+            <div class="flex">
+                <ul>
+                    <li class="files_liUp"><input type="file"></li>
+                    <li><img src="../../assets/image/LOGO.png" alt="" srcset=""></li>
+                    <li><img src="../../assets/image/LOGO.png" alt="" srcset=""></li>
+                </ul>
+                <div 
+                style="margin-top: 10px;width:155px;height:45px;border:1px solid #ffc630;color:#ffc630;text-align:center;font-size: 18px;line-height: 45px;">提交</div>
+            </div>
+        </div>
+    </div>
+  </div> 
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Swipe from "../../components/Swipe.vue";
+import mixin from "../../config/mixin";
+import { Action } from "vuex-class";
+import { Toast } from "vant";
+
+@Component({
+  components: {
+    Swipe
+  },
+  mixins: [mixin]
+})
+export default class User extends Vue {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/utils.scss";
+.rightTop{
+  height: 40px;background-color: rgb(247, 247, 247);font-size: 14px; border-bottom: 1px solid rgb(229, 229, 229);padding-left:10px;
+  line-height: 40px;border-bottom: 1px dashed #ddd;
+}
+.evaList{
+  padding:28px 30px;
+  .shopInfo{
+    width: 135px;margin-right: 25px;
+    h4{
+      font-size:18px;color: #000;font-weight: normal;
+    }
+  }
+  .shopText{
+    width: 720px;
+    .shopStar{
+      height: 26px;line-height:26px;margin-bottom:13px;
+      div{
+        margin-right:15px;font-size:14px;
+        img{
+          margin-right:10px;vertical-align: middle;
+        }
+      }
+    }
+    div{
+      textarea{
+        resize: none;width:720px;height: 130px;border:1px solid #ddd;padding:10px;margin-bottom: 10px;
+      }
+    }
+    div{
+      ul{
+          overflow: hidden;width:560px;
+        li{
+          width:68px;height: 68px;margin-right: 20px;float: left;margin-bottom: 10px;
+          img{
+            width:100%;height:100%;
+          }
+        }
+        .files_liUp{
+          background: url(../../assets/image/+.png) no-repeat center center;border:1px solid #ddd;
+        }
+        .files_liUp input{
+          width:100%;height: 100%;opacity: 0;
+        }
+      }
+      
+    }
+  }
+}
+</style>
---
+++ src/pages/index/reimburse.vue
@@ -0,0 +1,87 @@
+<template>
+  <!-- 规则 -->
+    <div style=" position: relative;">
+    <div style="background-color:rgba(0, 0, 0, 0.5);  z-index: 99999;position: fixed;width: 100%;height: 100vh;top:0;left:0;" >
+      <div class="flex flex-pack-center flex-align-center" style="height:100vh;">
+        <div class="flex flex-around-justify flex-align-center flex-v" style="background-color:#fff;width:780px;padding:20px; border-radius: 10px;    position: relative;">
+          <div style="position: absolute;top:10px;right:10px;">
+            <img src="../../assets/image/关闭按钮1.png" style="width:20px;height:20px;" />
+          </div>
+          <div class="flex btn_refund">
+            <div>售后类型</div>
+            <button :class="user_active == '0'? 'border_yellow':''" @click="reiClick('0')">未收到货,只退款</button>
+            <button :class="user_active == '1'? 'border_yellow':''" @click="reiClick('1')">退货退款</button>
+          </div>
+          <div v-if="user_active == '0'">
+              <div class="textbox">
+                  <p>提示：快递到达以后，请拒收快递，否则退款将被拒绝</p>
+                  <textarea placeholder="请填写退款理由"></textarea>
+              </div>
+              <div class="upPic">
+                <input type="file">
+              </div>
+          </div>
+          <div v-if="user_active == '1'">
+              退货退款
+          </div>
+
+          <div class="flex" style="margin-top:10px;">
+            <div 
+            style="width:150px;height:45px;background-color:#FCCB52;color:#fff;text-align: center;line-height:45px;margin-right:10px;">提交</div>
+          </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Swipe from "../../components/Swipe.vue";
+import mixin from "../../config/mixin";
+import { Action } from "vuex-class";
+import { Toast } from "vant";
+
+@Component({
+  components: {
+    Swipe
+  },
+  mixins: [mixin]
+})
+export default class User extends Vue {
+  user_active ='0';
+  reiClick(user_active){
+    this.user_active = user_active
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/utils.scss";
+.btn_refund{
+  div{
+    height:40px;line-height: 40px;width:100px;font-size:14px;margin-bottom: 20px;
+  }
+  button{
+      border:1px solid #e6e6e6;color: #000;height:40px;width:155px;margin-right:28px;font-size:14px;
+  }
+  .border_yellow{
+    border:1px solid #FCCB52;color: #FCCB52
+  }
+}
+.textbox{
+  border:1px solid #ddd;width:680px;margin-bottom:20px;
+  p{
+    line-height: 35px;border-bottom: 1px solid #ddd;padding-left:10px;
+  }
+  textarea{
+    resize: none;height:138px;border:none;width:100%;padding:10px;
+  }
+}
+.upPic{
+  padding-bottom: 20px;border-bottom: 1px solid #ddd;
+}
+
+</style>
